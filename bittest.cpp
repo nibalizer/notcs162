01d595c1dd1cd67eb3caa8ab2edc7a7c10e1623a
Spencer Krum <krum.spencer@gmail.com>
2011-03-28 04:23:58 -0700

    added bit and cintest

---
+++ bittest.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int one = 1;
+    printf('%d', one);
+    one = one << 1;
+    printf('%d', one);
+    return 0;
+}
+
---
+++ cintest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+    cin >> number;
+    cout << number << endl;
+    return 0;
+}
+
